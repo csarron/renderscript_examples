0e948cb218b8e56e52750915796ea97bb02c49f3
cmaster11 <cmaster11@gmail.com>
2016-04-14 18:24:04 +0200

    Fix comments

--- NativeAllocationMap/app/src/main/jni/allocation.h
+++ NativeAllocationMap/app/src/main/jni/allocation.h
@@ -1,29 +1,35 @@
 #include "apiLevel.h"
 #include "libRSLoader.h"
 
+// Function to get the allocation data pointer
+inline void *getAllocationPointer(JNIEnv *env, void *contextPtr, void *allocationPtr) {
 
-inline void * getAllocationPointer(JNIEnv *env, void *contextPtr, void *allocationPtr) {
-
-    void * mallocPtr;
-    RSFnPointers * fnPointers;
+    void *mallocPtr;
+    // Struct that contains RS lib function pointers
+    RSFnPointers *fnPointers;
 
     switch (getAPILevel(env)) {
-
+        // API 18 and 19 require direct memory access because
+        // they do not provide a "getPointer" function
         case 18:
-            mallocPtr = * (void **) (allocationPtr + 76);
+            mallocPtr = *(void **) (allocationPtr + 76);
             break;
         case 19:
-            mallocPtr = * (void **) (allocationPtr + 88);
+            mallocPtr = *(void **) (allocationPtr + 88);
             break;
+            // For API >= 21, AllocationGetPointer function is available
         case 21:
         case 22:
         case 23:
         case 24:
-            if((fnPointers = loadLibRSForAllocationGetPointer()) == NULL)
-                throw("rsAllocationGetPointer loading failed");
+            if ((fnPointers = loadLibRSForAllocationGetPointer()) == NULL)
+                throw ("rsAllocationGetPointer loading failed");
 
-            mallocPtr = fnPointers->AllocationGetPointer(contextPtr,allocationPtr,
-                                                         0,0,0,0,0,sizeof(size_t));
+            // void *rsi_AllocationGetPointer(Context *rsc, RsAllocation valloc,
+            //      uint32_t lod, RsAllocationCubemapFace face,
+            //      uint32_t z, uint32_t array, size_t *stride, size_t strideLen)
+            mallocPtr = fnPointers->AllocationGetPointer(contextPtr, allocationPtr, 0, 0, 0, 0, 0,
+                                                         sizeof(size_t));
 
             break;
         default:
